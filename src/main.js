c34bb406434787de6548399753a8d7a7bb402d6d
Adamya-Gupta <adamyagupta.2011@gmail.com>
2025-06-18 06:01:42 +0530

    basic url search

--- src/main.js
+++ src/main.js
@@ -10,14 +10,17 @@ if (started) {
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
+    autoHideMenuBar:true,
     width: 800,
     height: 600,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      webviewTag:true
     },
   });
 
   // and load the index.html of the app.
+  mainWindow.setAlwaysOnTop(true,'screen')
   if (MAIN_WINDOW_VITE_DEV_SERVER_URL) {
     mainWindow.loadURL(MAIN_WINDOW_VITE_DEV_SERVER_URL);
   } else {
--- src/renderer.js
+++ src/renderer.js
@@ -1,31 +1,47 @@
-/**
- * This file will automatically be loaded by vite and run in the "renderer" context.
- * To learn more about the differences between the "main" and the "renderer" context in
- * Electron, visit:
- *
- * https://electronjs.org/docs/tutorial/process-model
- *
- * By default, Node.js integration in this file is disabled. When enabling Node.js integration
- * in a renderer process, please be aware of potential security implications. You can read
- * more about security risks here:
- *
- * https://electronjs.org/docs/tutorial/security
- *
- * To enable Node.js integration in this file, open up `main.js` and enable the `nodeIntegration`
- * flag:
- *
- * ```
- *  // Create the browser window.
- *  mainWindow = new BrowserWindow({
- *    width: 800,
- *    height: 600,
- *    webPreferences: {
- *      nodeIntegration: true
- *    }
- *  });
- * ```
- */
-
 import './index.css';
 
-console.log('👋 This message is being logged by "renderer.js", included via Vite');
+const backButton = document.getElementById("back-button");
+const forwardButton = document.getElementById("forward-button");
+const reloadButton = document.getElementById("reload-button");
+const searchButton = document.getElementById("search-button");
+const newWindowButton = document.getElementById("new-window-button");
+const newTabButton = document.getElementById("new-tab-button");
+const goButton = document.getElementById("go");
+const urlInputField = document.getElementById("url-input");
+const webview = document.getElementById("webview");
+const tabsContainer = document.getElementById("tabs-container");
+
+const colorPicker = document.getElementById("color-picker");
+colorPicker.addEventListener("input", (event) => {
+    const selectedColor = event.target.value;
+    document.body.style.backgroundColor = selectedColor;
+    document.querySelector("#browser-tools").style.backgroundColor = selectedColor;
+    document.querySelector("#tabs-container").style.backgroundColor = selectedColor;
+})
+
+let tabs = [];
+let currentTabIndex = 0;
+
+function handleUrl(){
+    let url = "";
+    const inputUrl = urlInputField.value;
+    if(inputUrl.startsWith("http://") || inputUrl.startsWith("https://")){
+        url=inputUrl;
+    }
+    else{
+        url="http://"+inputUrl;
+    }
+    webview.src = url;
+    tabs[currentTabIndex].url=url;
+}
+urlInputField.addEventListener("keydown",(event)=>{
+    if(event.key ==="Enter"){
+        event.preventDefault();
+        handleUrl();
+    }
+});
+
+goButton.addEventListener("click", (event) => {
+  event.preventDefault();
+  handleUrl();
+});
